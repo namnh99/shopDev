25fb2f7e63f433da13ace68439553ae0b87b66f2
Hải Nam <hainam@Terminal.local>
2024-04-20 00:10:39 +0700

    remove unused file

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-// console.log(fs.readFileSync('logs/test_info.log.2024-02-16', { encoding: 'utf8', flag: 'r' }))
-
-// fs.unlink('logs/info.log.2024-02-16', (err) => {
-//   console.log(err)
-// })
-
-console.log(fs.accessSync('logs/test_info.log.2024-02-16', fs.constants.R_OK | fs.constants.W_OK))
